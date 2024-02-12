cb23c043630ab26d505a4e82555e66e5792459c7
antai <ai337@njit.edu>
2024-02-12 11:37:18 -0500

    first commit

--- app/calculations/__init__.py
+++ app/calculations/__init__.py
@@ -22,22 +22,45 @@ class Calculation:
         """Get the result of a calculation"""
         self.val2 = val2
 
+    def set_result(self, result):
+        """Get the result of a calculation"""
+        self.result = result
+
     def get_result(self):
         """Get the result of a calculation"""
         return self.result
 
 class Addition(Calculation):
+    """My Addition Calculation Class"""
+
     def __init__(self, val1, val2):
+        """concrete class constructor"""
         super().__init__(val1, val2)
+        self.set_result(addition(self.val1, self.val2))
+
 
 class Subtraction(Calculation):
+    """My subtraction Calculation Class"""
+
     def __init__(self, val1, val2):
+        """concrete class constructor"""
         super().__init__(val1, val2)
+        self.set_result(subtraction(self.val1, self.val2))
+
 
 class Multiplication(Calculation):
+    """My multiplication Calculation Class"""
+
     def __init__(self, val1, val2):
+        """concrete class constructor"""
         super().__init__(val1, val2)
+        self.set_result(multiplication(self.val1, self.val2))
+
 
 class Division(Calculation):
+    """My division Calculation Class"""
+
     def __init__(self, val1, val2):
+        """concrete class constructor"""
+        self.set_result(division(self.val1, self.val2))
         super().__init__(val1, val2)
